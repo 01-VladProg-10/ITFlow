c434ff720a3e0aba05c0fd62136b8f5425b1f8f0
Yana Pasternak <yana.pasternak@gmail.com>
2025-10-28 17:46:37 +0100

    Add Tailwind

---
+++ src/login.tsx
@@ -0,0 +1,8 @@
+export default function TestTailwind() {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-indigo-500">
+        <h1 className="text-white text-4xl font-bold">Tailwind працює!</h1>
+      </div>
+    );
+  }
+  
