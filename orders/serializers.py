20b6e07adda8a2b33ed27b893705591494c844f1
Vlad <polishchuk.vladyslav.work@gmail.com>
2025-10-31 13:27:51 +0100

    Ignore .idea directory

---
+++ orders/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from .models import Order
+from accounts.serializers import UserSerializer
+
+class OrderSerializer(serializers.ModelSerializer):
+    client = UserSerializer(read_only=True)
+    manager = UserSerializer(read_only=True)
+    developer = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Order
+        fields = [
+            'id',
+            'title',
+            'description',
+            'status',
+            'client',
+            'manager',
+            'developer',
+            'created_at',
+            'updated_at'
+        ]
+        read_only_fields = ['id', 'created_at', 'updated_at', 'client', 'manager', 'developer']
+
+    def create(self, validated_data):
+        user = self.context['request'].user
+        return Order.objects.create(client=user, **validated_data)
