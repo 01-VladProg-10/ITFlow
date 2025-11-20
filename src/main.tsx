b3aed26b3044b8935a8dfa8babfbdf45f11ea5c1
Yana Pasternak <yana.pasternak@gmail.com>
2025-11-20 16:49:49 +0100

    add UserSettings and ProgSettings

--- src/main.tsx
+++ src/main.tsx
@@ -7,12 +7,20 @@ import App from "./App";
 import Login from "./login";
 import UserDashboard from "./UserDashboard";
 import KontaktUser from "./pages/KontaktUser";
+import UserSettings from "./pages/UserSettings";
+import ProgSettings from "./pages/ProgSettings";
+import ManagerSettings from "./pages/ManagerSettings";
+
 
 const router = createBrowserRouter([
   { path: "/", element: <App /> },
   { path: "/login", element: <Login /> },
   { path: "/dashboard", element: <UserDashboard /> },
   { path: "/kontakt", element: <KontaktUser /> },
+  { path: "/ustawienia", element: <UserSettings /> },
+  { path: "/prog-ustawienia", element: <ProgSettings /> },
+  { path: "/manager-ustawienia", element: <ManagerSettings /> },
+
 ]);
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
