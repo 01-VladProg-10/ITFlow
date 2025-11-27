9d0136f6781d78db4ff58a42a7039c9daeb10466
Vlad <polishchuk.vladyslav.work@gmail.com>
2025-11-27 20:31:11 +0100

    Add endpoint to track developer progress at the start of a task

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,8 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from django.contrib.auth.models import Group
 from .models import User
+from orders.models import Order
+from orders.serializers import OrderSerializer
 from .serializers import UserSerializer, GroupSerializer
 
 
@@ -18,10 +20,11 @@ class UserViewSet(viewsets.ModelViewSet):
 
     @action(detail=False, methods=['get'], permission_classes=[IsAuthenticated])
     def me(self, request):
+        """Zwraca dane aktualnie zalogowanego użytkownika."""
         serializer = self.get_serializer(request.user)
         return Response(serializer.data)
 
-    @action(detail=False, methods=['post'])
+    @action(detail=False, methods=['post'], permission_classes=[AllowAny])
     def register(self, request):
         """
         Rejestracja nowego użytkownika.
@@ -29,9 +32,7 @@ class UserViewSet(viewsets.ModelViewSet):
         """
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-
         user = serializer.save()
-
         return Response(
             {
                 'message': 'User created successfully (assigned to "client" group automatically).',
@@ -40,11 +41,24 @@ class UserViewSet(viewsets.ModelViewSet):
             status=status.HTTP_201_CREATED
         )
 
-    @action(detail=False, methods=['get'])
-    def me(self, request):
-        """Zwraca dane aktualnie zalogowanego użytkownika."""
-        serializer = self.get_serializer(request.user)
-        return Response(serializer.data)
+    @action(detail=False, methods=['get'], permission_classes=[IsAuthenticated])
+    def dashboard(self, request):
+        """Zwraca dane użytkownika, jego grupy i najnowsze zamówienie."""
+        user = request.user
+        user_data = self.get_serializer(user).data
+
+        # grupy użytkownika
+        groups = [group.name for group in user.groups.all()]
+
+        # najnowsze zamówienie
+        latest_order = Order.objects.filter(client=request.user).order_by('-created_at').first()
+        order_data = OrderSerializer(latest_order).data if latest_order else None
+
+        return Response({
+            'user': user_data,
+            'groups': groups,
+            'latest_order': order_data
+        })
 
 
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
